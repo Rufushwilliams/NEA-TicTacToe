eb6e4840a9f4a92a5f52d146aea9c1ad4bb6f5de
Rufushwilliams <rhwilliams@perse.co.uk>
2022-04-28 10:32:11 +0100

    starting to implement basic stuff

--- Game.py
+++ Game.py
@@ -2,17 +2,30 @@
 class Game:
 
     def __init__(self):
-        pass
+        self.blank = "-"
+        self.pturn = 1
+        self.psymbol = {1: "X", 2: "O"}
+        self.board = [["-", "-", "-"],["-", "-", "-"],["-", "-", "-"]]
 
     def __repr__(self):
-        pass
+        return self.board
 
     def play(self,row,col):
-        pass
+        if self.board[row][col] == self.blank:
+            self.board[row][col] = self.psymbol[self.pturn]
+            if self.pturn == 1:
+                self.pturn = 2
+            else:
+                self.pturn = 1
+        else:
+            # location has already been played
+            pass
     
     @property
     def winner(self):
-        pass
+        for row in range(3):
+            if self.board[row][0] == self.board[row][1] and self.board[row][0] == self.board[row][2]:
+                pass
 
 if __name__ == "__main__":
     pass
--- Ui.py
+++ Ui.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from Game import Game
 
 class Ui(ABC):
 
@@ -18,4 +19,13 @@ class Terminal(Ui):
         pass
 
     def run(self):
-        pass
+        game = Game()
+        while True:
+            print(game)
+            print(f"It is player {game.pturn}s turn!")
+            print("Please enter the row and column you want to play in")
+            row = int(input("Enter the row: "))
+            col = int(input("Enter the column: "))
+            game.play(row, col)
+            if game.winner != False:
+                break
